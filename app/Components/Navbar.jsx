2ac3de8da8daeabd8e1d31a1bf4de9124cf528aa
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-19 03:14:39 +0600

    Navbar and table form added

---
+++ app/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-wrap py-3 justify-around'>
+      <h1 className='text-lg font-semibold'>Todo App</h1>
+      <ul className='flex gap-[40px] '>
+        <li>Home</li>
+        <li>Products</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      
+    </div>
+  );
+};
+
+export default Navbar;
