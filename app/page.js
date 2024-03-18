9b2eb18a85db9f12b6288360d86801eb6f0f69a4
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-19 03:36:17 +0600

    Todo section Done

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Todo from "./Components/Todo";
+
 export default function Home() {
   return (
     <>
@@ -6,68 +8,32 @@ export default function Home() {
         <textarea name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full rounded-md"></textarea>
         <button type="submit" className="bg-orange-600 text-white py-3 px-11 rounded-md">Add Todo</button>
       </form>
-{/* table from flowbite */}
+      {/* table from flowbite */}
       <div className="relative overflow-x-auto mt-24 w-[60%] mx-auto">
         <table className="w-full text-sm text-left rtl:text-right">
           <thead className="text-xs ">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Product name
+                ID
               </th>
               <th scope="col" className="px-6 py-3">
-                Color
+                Title
               </th>
               <th scope="col" className="px-6 py-3">
-                Category
+                Description
               </th>
               <th scope="col" className="px-6 py-3">
-                Price
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr className="">
-              <th scope="row" className="px-6 py-4 font-medium">
-                Apple MacBook Pro 17"
-              </th>
-              <td className="px-6 py-4">
-                Silver
-              </td>
-              <td className="px-6 py-4">
-                Laptop
-              </td>
-              <td className="px-6 py-4">
-                $2999
-              </td>
-            </tr>
-            <tr className="">
-              <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap ">
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">
-                White
-              </td>
-              <td className="px-6 py-4">
-                Laptop PC
-              </td>
-              <td className="px-6 py-4">
-                $1999
-              </td>
-            </tr>
-            <tr className="">
-              <th scope="row" className="px-6 py-4 font-medium">
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">
-                Black
-              </td>
-              <td className="px-6 py-4">
-                Accessories
-              </td>
-              <td className="px-6 py-4">
-                $99
-              </td>
-            </tr>
+            <Todo></Todo>
+            <Todo></Todo>
+            <Todo></Todo>
           </tbody>
         </table>
       </div>
