b2acf32df363ea3c101b57033d5abd390318bcdb
rahathasanDev <devrahat.hasan@gmail.com>
2024-03-20 05:36:14 +0600

    onChangeHandler and submithandler added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,27 @@
+"use client"
+import { useState } from "react";
 import Todo from "./Components/Todo";
 
 export default function Home() {
+  const [formData,setFormData] = useState({
+    title:"",
+    description:"",
+  });
+  const onChangeHandler = (e) =>{
+    const name = e.target.name ;
+    const value = e.target.value;
+    setFormData(form => ({...form,[name]:value}));
+    console.log(formData);
+    } 
+    const onSubmitHandler = async (e)=>{
+      e.preventDefault();
+
+    }
   return (
     <>
-      <form className="flex flex-col items-start gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto" >
-        <input type="text" name="title" placeholder="Enter Title" className="px-3 py-2 border-2 w-full rounded-md" />
-        <textarea name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full rounded-md"></textarea>
+      <form onSubmit={onSubmitHandler} className="flex flex-col items-start gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto" >
+        <input value={formData.title} onChange={onChangeHandler} type="text" name="title" placeholder="Enter Title" className="px-3 py-2 border-2 w-full rounded-md" />
+        <textarea value={formData.description} onChange={onChangeHandler} name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full rounded-md"></textarea>
         <button type="submit" className="bg-orange-600 text-white py-3 px-11 rounded-md">Add Todo</button>
       </form>
       {/* table from flowbite */}
